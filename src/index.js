1755ede3b89bc5413cb684a005e4ec432bc721fb
Tim Flood <tflood9@gmail.com>
2021-02-19 14:00:41 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  //const submitButton = document.getElementById("submit_button");
+  const tasks = document.getElementById("tasks");
+  const form = document.getElementById("create-task-form");
+  //submitButton.addEventListener("click", handleClickEvent);
+
+  form.addEventListener("submit", handleSubmit);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let value = event.target[0].value;
+    tasks.innerHTML += `<li>${value}</li>`;
+  }
 });
